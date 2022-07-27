6be8fadb37c8b6a263e0fd76363e6827a8219c4d
Matthew Snelgrove <matthew.s13542@gmail.com>
2022-07-27 18:44:31 -0400

    connected app to firebase

--- app/src/main/java/com/example/b07project/MainActivity.java
+++ app/src/main/java/com/example/b07project/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = db.getReference("woke message");
+        myRef.setValue("Something woke");
+
     }
 }
