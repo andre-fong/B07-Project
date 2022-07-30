4788ffeb94fa49c5f4dc658007481fb404e4b3c8
Andre Fong <andre.fong@mail.utoronto.ca>
2022-07-30 16:55:04 -0400

    Add intent from customer login to signup activity
    Add email String extra from customer login -> signup
    Change text size of user input fields to 18sp from 14sp

--- app/src/main/java/com/example/b07project/MainActivity.java
+++ app/src/main/java/com/example/b07project/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.b07project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,13 +17,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.setTitle("Welcome to App!");
+
+        // Create invisible clickable buttons
+        Button adminButton = (Button)findViewById(R.id.ctrAdminLink);
+        Button signupButton = (Button)findViewById(R.id.ctrCreateAccount);
+        adminButton.setBackgroundColor(Color.TRANSPARENT);
+        signupButton.setBackgroundColor(Color.TRANSPARENT);
     }
 
-    public void admin_login(View v) {
+    // Start AdminLoginActivity
+    public void adminLogin(View v) {
         // TODO: Intent to AdminLoginActivity
     }
 
-    public void create_acc(View v) {
-        // TODO: Intent to SignupActivity
+    // Start SignupActivity
+    public void createAcc(View v) {
+        String email = ((TextView)findViewById(R.id.ctrEmailField)).getText().toString();
+        Intent i = new Intent(this, SignupActivity.class);
+        i.putExtra("Email", email);
+        startActivity(i);
+    }
+
+    // Authentication
+    public void submitReq(View v) {
+        String email = ((TextView)findViewById(R.id.ctrEmailField)).getText().toString();
+        String pwd = ((TextView)findViewById(R.id.ctrPasswordField)).getText().toString();
+
+        // TODO: Firebase pros only
+
+        // If email and pwd valid:
+            // Create new Customer obj
+            // Start MainAppActivity
+        // Else
+            // Toast.alert Invalid login credentials
     }
 }
--- app/src/main/java/com/example/b07project/SignupActivity.java
+++ app/src/main/java/com/example/b07project/SignupActivity.java
@@ -2,7 +2,10 @@ package com.example.b07project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        // Get username data from MainActivity
+        Intent i = getIntent();
+        EditText t = (EditText)findViewById(R.id.ctrEmailSignup);
+        t.setText(i.getStringExtra("Email"));
     }
+
+
 }
