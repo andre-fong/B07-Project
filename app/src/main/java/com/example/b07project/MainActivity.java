32f5a50b9035cea03560cacd6d542502d397dca1
Andre Fong <andre.fong@mail.utoronto.ca>
2022-07-29 22:46:46 -0400

    Create skeleton for MainActivity
    Added to strings.xml, colors.xml
    Modified AndroidManifest
    Added TODO for button methods

--- app/src/main/java/com/example/b07project/MainActivity.java
+++ app/src/main/java/com/example/b07project/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.b07project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -13,10 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setTitle("Welcome to App!");
+    }
 
-        FirebaseDatabase db = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = db.getReference("woke message");
-        myRef.setValue("Something woke");
+    public void admin_login(View v) {
+        // TODO: Intent to AdminLoginActivity
+    }
 
+    public void create_acc(View v) {
+        // TODO: Intent to SignupActivity
     }
 }
