eccc7a9fb00ebd7e832e9fd40d1daa89d916c753
victo <victorghurst@gmail..com>
2022-08-09 16:22:50 -0400

    add time-check for schedule-events activity

--- app/src/main/java/com/example/b07project/DatePickerFragment.java
+++ app/src/main/java/com/example/b07project/DatePickerFragment.java
@@ -33,7 +33,7 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
     public void onDateSet(DatePicker view, int year, int month, int day) {
         // Do something with the date chosen by the user
 
-        String date = month + "/" + day + "/" + year;
+        String date = (month+1) + "/" + day + "/" + year;
         Intent intent = new Intent(getActivity(), ScheduleEventActivity.class);
         intent.putExtra("date_string", date);
         intent.putExtra("time_string",this.getArguments().getString("time_string"));
